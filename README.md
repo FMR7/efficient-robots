129822b2b3a547ad9e7bf453f1940db0d714f963
Fernando <14892931+FMR7@users.noreply.github.com>
2022-06-23 15:08:35 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Factorio Efficient Robots
+Improves energy consumption of both, logistic and construction, robots
+
+# Mod Page
+https://mods.factorio.com/mod/efficient-robots
---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw["logistic-robot"]["logistic-robot"].energy_per_tick = "0.01kJ"
+data.raw["logistic-robot"]["logistic-robot"].energy_per_move = "1kJ"
+data.raw["construction-robot"]["construction-robot"].energy_per_tick = "0.01kJ"
+data.raw["construction-robot"]["construction-robot"].energy_per_move = "1kJ"
