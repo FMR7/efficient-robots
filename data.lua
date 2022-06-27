bc5cca90f5412d7bf90e1a1304b5c3eec3947afa
Fernando <14892931+FMR7@users.noreply.github.com>
2022-06-28 01:09:11 +0200

    Startup efficiency setting

--- data.lua
+++ data.lua
@@ -1,4 +1,13 @@
-data.raw["logistic-robot"]["logistic-robot"].energy_per_tick = "0.01kJ"
-data.raw["logistic-robot"]["logistic-robot"].energy_per_move = "1kJ"
-data.raw["construction-robot"]["construction-robot"].energy_per_tick = "0.01kJ"
-data.raw["construction-robot"]["construction-robot"].energy_per_move = "1kJ"
+local efficiency = settings.startup["efficient-robots-efficiency"].value
+local defaultEnergyPerTick = 0.05
+local defaultEnergyPerMove = 5
+
+if (efficiency > 0) then
+    local energyPerTick = defaultEnergyPerTick - (defaultEnergyPerTick * efficiency / 100)
+    local energyPerMove = defaultEnergyPerMove - (defaultEnergyPerMove * efficiency / 100)
+
+    data.raw["logistic-robot"]["logistic-robot"].energy_per_tick = energyPerTick .. "kJ"
+    data.raw["logistic-robot"]["logistic-robot"].energy_per_move = energyPerMove .. "kJ"
+    data.raw["construction-robot"]["construction-robot"].energy_per_tick = energyPerTick .. "kJ"
+    data.raw["construction-robot"]["construction-robot"].energy_per_move = energyPerMove .. "kJ"
+end
