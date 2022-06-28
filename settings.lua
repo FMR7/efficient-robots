c75a6d3e723aafa92c10673076060530222a679f
Fernando <14892931+FMR7@users.noreply.github.com>
2022-06-28 13:13:02 +0200

    Startup efficiency setting

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({{
+    type = "double-setting",
+    name = "efficient-robots-efficiency",
+    localised_name = "Robot efficiency [img=info]",
+    localised_description = "To get base game efficiency use 0",
+    setting_type = "startup",
+    minimum_value = 0,
+    maximum_value = 100,
+    default_value = 80
+}})
